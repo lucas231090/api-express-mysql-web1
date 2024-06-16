131eda2b90bea2d75a587f0588d6229bb868c35b
Lucas <souza.lucasj@gmail.com>
2024-06-15 21:45:48 -0300

    add public

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
 const app = express();
-
+const path = require("path");
 const url = "http://localhost";
 const port = 3000;
 
@@ -16,6 +16,14 @@ db.testConnection().catch((err) => {
 
 app.use(express.json());
 app.use(routes);
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response
+    .status(200)
+    .sendFile(path.join(__dirname + "/../public/pages/index.html"));
+});
 
 app.listen(port, () => {
   console.log(`O servidor está rodando em ${url} ${port}`);
--- src/routes.js
+++ src/routes.js
@@ -5,23 +5,16 @@ const DepartmentController = require("./app/controllers/Department");
 const routes = Router();
 
 // Employee
-
 routes.get("/employees", EmployeeController.index);
-
 routes.get("/employees/:id", EmployeeController.show);
-
 routes.post("/employees", EmployeeController.store);
-
 routes.put("/employees/:id", EmployeeController.update);
-
 routes.delete("/employees/:id", EmployeeController.delete);
 
 // Departments
 
 routes.get("/departments", DepartmentController.index);
-
 routes.post("/departments", DepartmentController.store);
-
 routes.put("/departments/:id", DepartmentController.update);
 
 module.exports = routes;
